871a9864d0803ba7b41d919f5415bdc153471396
Lourival Rodrigues <lourivalformal@gmail.com>
2021-12-16 13:51:21 -0300

    podcasts

--- web-quest/src/scripts/podcasts.js
+++ web-quest/src/scripts/podcasts.js
@@ -1,18 +1,16 @@
-var btn = document.querySelector('#btnReadMore');
-var container = document.querySelector('.question-on-corona');
+var btn = document.querySelectorAll('.btnReadMore');
+var divQuest = document.querySelector('.question-on-corona');
 
-btn.addEventListener('click', function() {
-
-  if(container.style.display === 'block') {
-    container.style.display = 'none';
-    btn.innerHTML = 'Leia Mais';
-  }else {
-    container.style.display = 'block';
-    btn.innerHTML = 'Leia Menos';
-  }
+btn.forEach(button => {
+    button.addEventListener('click', function () {
+        if (divQuest.style.display === 'block') {
+            divQuest.style.display = 'none';
+            btn.innerHTML = 'Leia Mais';
+        } else {
+            divQuest.style.display = 'block';
+            btn.innerHTML = 'Leia Menos';
+        }
+    }
+    );
 }
-  );
-
-
-
-
+)
